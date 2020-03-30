761ab09da202c21349e5497b7cc4b77ba8a87618
Pawel <pawel.gladysz1999@onet.pl>
2020-03-30 03:23:34 +0200

    Final result of the project :)

---
+++ FootballersForm/FootballersForm/Footballer.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballersForm
+{
+    class Footballer
+    {
+        private string _name;
+        private string _surname;
+        private byte _age;
+        private float _weight;
+        
+
+        public Footballer(string _name, string _surname, byte _age, float _weight)
+        {
+            this._name = _name;
+            this._surname = _surname;
+            this._age = _age;
+            this._weight = _weight;
+        }
+        public bool isInList(Footballer pilkarz)
+        {
+            if (pilkarz._name != _name) return false;
+            if (pilkarz._surname != _surname) return false;
+            if (pilkarz._age != _age) return false;
+            if (pilkarz._weight != _weight) return false;
+            return true;
+        }
+        public override string ToString()
+        {
+            return _name + " " +   _surname + " " +  + _age + " lat " + _weight + " kg wagi ";
+        }
+        public string FormatSaving()
+        {
+            return _name + ";" + _surname + ";" + _age + ";" + _weight;
+        }
+        public static Footballer FootballerReadyToAdd(string s)
+        {
+            string name, surname;
+            byte age;
+            float weight;
+            var array = s.Split(';');
+            if (array.Length == 4)
+            {
+                {
+                    name = array[0];
+                    surname = array[1];
+                    age = Convert.ToByte(array[2]);
+                    weight = float.Parse(array[3]);
+                    return new Footballer(name, surname, age, weight);
+                }
+            }
+            throw new Exception("Błędny format danych z pliku");
+        }
+        public string getName()
+        {
+            return _name;
+        }
+        public void setName(string _name)
+        {
+            this._name = _name;
+        }
+        public string getSurname()
+        {
+            return _surname;
+        }
+        public void setSurname(string _surnname)
+        {
+            this._surname = _surnname;
+        }
+        public byte getAge()
+        {
+            return _age;
+        }
+        public void setAge(byte _age)
+        {
+            this._age = _age;
+        }
+        public float getWeight()
+        {
+            return _weight;
+        }
+        public void setWeight(byte _weight)
+        {
+            this._weight = _weight;
+        }
+    }
+}
--- FootballersForm/FootballersForm/MainWindow.xaml.cs
+++ FootballersForm/FootballersForm/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,17 +16,16 @@ using System.Windows.Shapes;
 
 namespace FootballersForm
 {
-    /// <summary>
-    /// Logika interakcji dla klasy MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             TextBoxError.BrushForAll = Brushes.Red;
             InitializeComponent();
-
-
+            for (int i = 15; i < 70; i++)
+            {
+                cbAge.Items.Add(i);
+            }
         }
 
         private bool isNoEmpty(TextBoxError tb)
@@ -42,14 +42,146 @@ namespace FootballersForm
 
         private void buttonOK_Click(object sender, RoutedEventArgs e)
         {
-            //zwróć uwagę na różnicę między operatorem logicznym & - wyznacza obydwa operandy i liczy wynik
-            //a operatorem warunkowym && który w przypadku lewego operandu fałszywego nie wyznacza prawego perandu
-            //i wówczas nie zgłosi błędu w drugim polu!!!
-            if (isNoEmpty(textBox) & isNoEmpty(textBox1))
+            if (isNoEmpty(tbName) & isNoEmpty(tbSurname))
             {
-                //tu poprawnie mamy wpisane dane
+                var currentFootballer = new Footballer(tbName.Text.Trim(), tbSurname.Text.Trim(), Convert.ToByte(cbAge.Text), (float)slweight.Value);
+                bool isOnList = false;
+                foreach (var p in lb_LisOfPlayers.Items)
+                {
+                    var pilkarz = p as Footballer;
+                    if (pilkarz.isInList(currentFootballer))
+                    {
+                        isOnList = true;
+                        break;
+                    }
+                }
+                if(isOnList==false)
+                {
+                    lb_LisOfPlayers.Items.Add(currentFootballer);
+                    ResetValues();
+                }
+                else
+                {
+                    var dialog = MessageBox.Show("Piłkarz którego chcesz dodać jest już na liście!", "Uwaga", MessageBoxButton.OK);
+                    ResetValues();
+                } 
             }
 
+        }
+
+        private void slweight_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (sender != null)
+            {
+                tbweight.Text = Math.Round(slweight.Value,1).ToString() + " kg";
+            }
+        }
+
+        private void RemoveFootballer(object sender, RoutedEventArgs e)
+        {
+            if (lb_LisOfPlayers.SelectedIndex == -1)
+            {
+                var dialog = MessageBox.Show("Nie wybrales nikogo do usuniecia", "Uwaga", MessageBoxButton.OK);
+            }
+            else { 
+                var dialog = MessageBox.Show("Czy na pewno usunać piłkarza?", "Uwaga", MessageBoxButton.OKCancel);
+            if (dialog == MessageBoxResult.OK)
+            {
+                lb_LisOfPlayers.Items.Remove(lb_LisOfPlayers.SelectedItem);
+                ResetValues();
+            }
+            }
+        }
+
+        private void ModifyFootballer(object sender, RoutedEventArgs e)
+        {
+            if (lb_LisOfPlayers.SelectedIndex != -1)
+            {
+                var dialog = MessageBox.Show("Czy na pewno chcesz modyfikować dane piłkarza?", "Uwaga", MessageBoxButton.OKCancel);
+                int index = 0;
+                if (dialog == MessageBoxResult.OK)
+                {
+
+                    index = lb_LisOfPlayers.SelectedIndex;
+                   // lb_LisOfPlayers.SelectedIndex = -1;
+                    if (index != -1 && isNoEmpty(tbName) & isNoEmpty(tbSurname))
+                    {
+                        var currentFootballer = new Footballer(tbName.Text.Trim(), tbSurname.Text.Trim(), Convert.ToByte(cbAge.Text), (float)slweight.Value);
+                        bool isOnList = false;
+                        foreach (var p in lb_LisOfPlayers.Items)
+                        {
+                            var pilkarz = p as Footballer;
+                            if (pilkarz.isInList(currentFootballer))
+                            {
+                                isOnList = true;
+                                break;
+                            }
+                        }
+                        if (isOnList == false)
+                        {
+                            lb_LisOfPlayers.Items.Remove(lb_LisOfPlayers.SelectedItem);
+                            lb_LisOfPlayers.Items.Insert(index, currentFootballer);
+                        }
+                        else
+                        {
+                            var dialog2 = MessageBox.Show("Już taki piłkarz jest na liście, wprowadź innego!", "Uwaga", MessageBoxButton.OK);
+                        }
+                    }
+                    ResetValues();
+                }
+            }
+        }
+
+        private void IsSelect(object sender, SelectionChangedEventArgs e)
+        {
+            //lb_LisOfPlayers.SelectedIndex != -1
+            if (sender != null && lb_LisOfPlayers.SelectedIndex != -1)
+            {
+                Footballer player = (Footballer)lb_LisOfPlayers.SelectedItem;
+                tbName.Text = player.getName();
+                tbSurname.Text = player.getSurname();
+                slweight.Value = player.getWeight();
+                cbAge.Text = player.getAge().ToString();
+
+            }
+        }
+        private void ResetValues()
+        {
+            tbName.Text = "";
+            tbSurname.Text = "";
+            slweight.Value = 50;
+            cbAge.SelectedIndex = 0;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            int countOfPlayers = lb_LisOfPlayers.Items.Count;
+            string path = @"E:\SemestrIV\.NET\FootballersForm\FootballersForm\dane.txt";
+            System.IO.File.WriteAllText(path, string.Empty);
+
+            for (int i = 0; i < countOfPlayers; i++)
+            {
+                var temp  = lb_LisOfPlayers.Items[i] as Footballer;
+                File.AppendAllText(path,
+                temp.FormatSaving() + Environment.NewLine);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(@"E:\SemestrIV\.NET\FootballersForm\FootballersForm\dane.txt");
+                foreach (string line in lines)
+                {
+                    lb_LisOfPlayers.Items.Add(Footballer.FootballerReadyToAdd(line));
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Nie udało się wczytać danych z pliku! Sprawdź czy plik istenieje i jego poporawność");
+            }
+            
         }
     }
 }
